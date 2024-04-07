6999a2174383ffcc6dbd1049764f69271b01a014
ShadmanAhm3d <shadman01010@gmail.com>
2024-04-07 21:06:40 +0530

    Pushback

---
+++ day4/endses/script.js
@@ -0,0 +1,16 @@
+
+function sun(a, b, print) {
+  const ans = a + b;
+  print(ans)
+}
+
+function print(x) {
+  console.log('**   ' x, '***')
+}
+sun(3, 4, print)
+
+
+/* async callback
+promises
+maps */
+
